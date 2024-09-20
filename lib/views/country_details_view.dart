31d709f5025a520bc1bdd43403200526c9c7bec0
Gabriel Bitencourt Marin <gabrielbitercourt@gmail.com>
2024-09-20 11:05:40 -0400

    Melhore o visual da página de detalhes

--- lib/views/country_details_view.dart
+++ lib/views/country_details_view.dart
@@ -11,60 +11,57 @@ class CountryDetailsView extends GetView<CountryDetailsController> {
     return controller.obx(
       (state) => FutureBuilder(
         builder: (context, snapshot) {
-          if (snapshot.connectionState != ConnectionState.done) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          if (snapshot.hasError) {
-            return Center(
-              child: Text('Erro ao carregar o conteúdo: ${snapshot.error}'),
-            );
-          }
           return Theme(
             data: ThemeData.from(colorScheme: snapshot.data!),
-            child: Scaffold(
-              appBar: AppBar(
-                backgroundColor: context.theme.colorScheme.inversePrimary,
-                title: Text(state.name),
-              ),
-              body: ListView(
-                padding: const EdgeInsets.all(8),
-                children: [
-                  Card.filled(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 24.0),
-                      child: Column(
-                        children: [
-                          Image.network(state.flag),
-                          Text('Região: ${state.region}'),
-                          Text('Capital: ${state.capital}'),
-                        ],
-                      ),
-                    ),
+            child: Builder(
+              builder: (context) {
+                return Scaffold(
+                  appBar: AppBar(
+                    backgroundColor: context.theme.colorScheme.inversePrimary,
+                    title: Text(state.name),
                   ),
-                  Card.filled(
-                    child: ListTile(
-                      title: Column(
-                        children: [
-                          Text('Latitude: ${state.latitude}'),
-                          Text('Longitude: ${state.longitude}'),
-                        ],
+                  body: ListView(
+                    padding: const EdgeInsets.all(8),
+                    children: [
+                      Card.filled(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 24.0),
+                          child: Column(
+                            children: [
+                              Image.network(state.flag),
+                              Text('Região: ${state.region}'),
+                              Text('Capital: ${state.capital}'),
+                            ],
+                          ),
+                        ),
                       ),
-                    ),
+                      Card.filled(
+                        child: ListTile(
+                          title: Column(
+                            children: [
+                              Text('Latitude: ${state.latitude}'),
+                              Text('Longitude: ${state.longitude}'),
+                            ],
+                          ),
+                        ),
+                      ),
+                      FilledButton(
+                        onPressed: () async {
+                          final Uri url = Uri.parse(state.map);
+                          if (!await launchUrl(url)) {
+                            throw Exception('Could not launch $url');
+                          }
+                        },
+                        child: const Text('Ver no Mapa'),
+                      )
+                    ],
                   ),
-                  FilledButton(
-                    onPressed: () async {
-                      final Uri url = Uri.parse(state.map);
-                      if (!await launchUrl(url)) {
-                        throw Exception('Could not launch $url');
-                      }
-                    },
-                    child: const Text('Ver no Mapa'),
-                  )
-                ],
-              ),
+                );
+              },
             ),
           );
         },
+        initialData: context.theme.colorScheme,
         future: ColorScheme.fromImageProvider(
           provider: NetworkImage(state!.flag),
           dynamicSchemeVariant: DynamicSchemeVariant.content,
